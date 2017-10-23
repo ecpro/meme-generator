2d24e78be35e2af4006b23211a638013830f1604
Piyush Ravi <mails4ipiyush@gmail.com>
2017-10-24 01:07:02 +0530

    add canvas default text

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-
+// canvas configuration
 const canvas = document.querySelector('.memeImage canvas');
 const ctx = canvas.getContext('2d');
+const height = canvas.height;
+const width = canvas.width;
 
-const img = new Image();
-img.src = 'img1.jpg';
+// initial font configuration 
+ctx.font = '30px Impact';
+ctx.fillStyle = 'white';
+ctx.strokeStyle = 'black';
+ctx.textAlign = 'center';
 
-img.addEventListener('load', function() {
-  if(ctx) {
-  	ctx.drawImage(img, 0,0);
-  }
-}, false);
+window.onload = reDrawMeme;
+
+function reDrawMeme(image, topLineText, bottomLineText, topX, topY, bottomX, bottomY) {
+    topLineText = topLineText || 'Your Image Here';
+    topX = topX || width / 2;
+    topY = topY || height / 2;
+
+    ctx.fillText(topLineText, topX, topY);
+    ctx.strokeText(topLineText, topX, topY);
+}
+
+reDrawMeme();
